2bfae6dbed198f29fcbf4c01ade3d42c91eb2ad3
Jonathan Ware <jonathan.ware02@gmail.com>
2023-05-02 09:50:29 -0500

    create exercises sheet for SQL database into

---
+++ db_tables_exercises.sql
@@ -0,0 +1,24 @@
+#Open MySQL Workbench and login to the database server
+#Save your work in a file named db_tables_exercises.sql
+
+#List all the databases
+SHOW DATABASES;
+
+#Show the currently selected database
+
+
+#List all tables in the database
+
+#Write the SQL code to switch the employees database
+
+#List all the tables in the database
+
+#Explore the employees table. What different data types are present on this table?
+
+#Which table(s) do you think contain a numeric type column? (Write this question and your answer in a comment)
+
+#Which table(s) do you think contain a string type column? (Write this question and your answer in a comment)
+
+#Which table(s) do you think contain a date type column? (Write this question and your answer in a comment)
+
+#What is the relationship between the employees and the departments tables? (Write this question and your answer in a comment)
