44c9a96f1a74436cde9b245ea462c4ea955ff5e4
Jonathan Ware <jonathan.ware02@gmail.com>
2023-05-04 11:09:58 -0500

    created join exercise file

---
+++ join_exercises.sql
@@ -0,0 +1,21 @@
+SHOW DATABASES;
+
+USE employees;
+
+SELECT*
+FROM employees;
+
+
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS full_name, d.dept_name
+FROM employees AS e
+JOIN dept_emp AS de
+  ON de.emp_no = e.emp_no
+JOIN departments AS d
+  ON d.dept_no = de.dept_no
+
+
+
+
+
+
+
